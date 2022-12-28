98eb9e56409f4f5a8344016073ada26ed09f67b7
Tedtya Rady <tedtyarady@gmail.com>
2022-12-28 19:45:00 +1100

    Initial Commit

---
+++ CarDealerNew/CarDealerNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarDealerNewApp.swift
+//  CarDealerNew
+//
+//  Created by Tedtya rady on 28/12/22.
+//
+
+import SwiftUI
+
+@main
+struct CarDealerNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarDealerNew/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CarDealerNew
+//
+//  Created by Tedtya rady on 28/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
